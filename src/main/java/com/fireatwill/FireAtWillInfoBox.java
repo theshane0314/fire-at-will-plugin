ebfd0d6fb306e97b98e03baa522352a783444e45
theshane0314 <theshane0314@gmail.com>
2026-05-15 14:14:34 -0400

    Move plugin out of net.runelite package (plugin-hub requirement)

--- src/main/java/com/fireatwill/FireAtWillInfoBox.java
+++ src/main/java/com/fireatwill/FireAtWillInfoBox.java
@@ -22,7 +22,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package net.runelite.client.plugins.fireatwill;
+package com.fireatwill;
 
 import java.awt.Color;
 import java.awt.image.BufferedImage;
--- src/test/java/com/fireatwill/FireAtWillPluginTest.java
+++ src/test/java/com/fireatwill/FireAtWillPluginTest.java
@@ -22,7 +22,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package net.runelite.client.plugins.fireatwill;
+package com.fireatwill;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
